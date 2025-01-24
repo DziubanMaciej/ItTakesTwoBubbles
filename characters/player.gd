8b878719da9870d1fe66c9bc24cac89452f6054c
UnFriend-PL <szymonecki1233@gmail.com>
2025-01-24 23:24:46 +0100

    fixed player jumping when "W" is pressed

--- characters/player.gd
+++ characters/player.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Projectile container is a parent node for each projectile. We need it to be
 # relative to world, not the player.
-@export var projectile_container : Node2D
-
+@export var projectile_container: Node2D
 @export var player = "player0"
 var action_left = str(player, "_left")
 var action_right = str(player, "_right")
@@ -16,23 +14,32 @@ var action_shoot = str(player, "_shoot")
 
 @onready var scene_projectile = preload("res://objects/projectile.tscn")
 
+# Flag to track if the jump button is being held
+var jump_held = false
+
 func _process(delta: float) -> void:
-    if Input.is_action_just_pressed(action_shoot):
-        var projectile = scene_projectile.instantiate()
-        projectile_container.add_child(projectile)
-        projectile.global_position = $Emitter.global_position
+	# Handle shooting projectiles
+	if Input.is_action_just_pressed(action_shoot):
+		var projectile = scene_projectile.instantiate()
+		projectile_container.add_child(projectile)
+		projectile.global_position = $Emitter.global_position
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed(action_jump) and is_on_floor():
+	# Handle jumping
+	if Input.is_action_pressed(action_jump):
+		jump_held = true
+	else:
+		jump_held = false
+
+	# Jump when on the floor and the jump button is held
+	if is_on_floor() and jump_held:
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Handle horizontal movement
 	var direction := Input.get_axis(action_left, action_right)
 	if direction:
 		velocity.x = direction * SPEED
