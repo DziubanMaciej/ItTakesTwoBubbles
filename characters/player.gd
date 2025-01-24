5c8690f4598e3b8dd5e428465a549962d016c668
Dziuban <mdziuban@amd.com>
2025-01-24 21:09:52 +0100

    Use per-player keymaps

--- characters/player.gd
+++ characters/player.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@export var player = "player0"
+var action_left = str(player, "_left")
+var action_right = str(player, "_right")
+var action_jump = str(player, "_jump")
 
 func _physics_process(delta: float) -> void:
     # Add the gravity.
@@ -11,12 +15,12 @@ func _physics_process(delta: float) -> void:
         velocity += get_gravity() * delta
 
     # Handle jump.
-    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+    if Input.is_action_just_pressed(action_jump) and is_on_floor():
         velocity.y = JUMP_VELOCITY
 
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
-    var direction := Input.get_axis("ui_left", "ui_right")
+    var direction := Input.get_axis(action_left, action_right)
     if direction:
         velocity.x = direction * SPEED
     else:
